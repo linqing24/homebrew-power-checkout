b33c06920fa2edfc09020e88fb761ef7462780a4
linqing <linqing.me@gmail.com>
2024-11-11 12:50:36 +0800

    fix: fix sha256 hash

--- Formula/power-checkout.rb
+++ Formula/power-checkout.rb
@@ -2,7 +2,7 @@ class PowerCheckout < Formula
     desc "Quickly switch to another branch"
     homepage "https://github.com/linqing24/power-checkout"
     url "https://github.com/linqing24/power-checkout/releases/download/1.0.0/power-checkout.tar.gz"
-    sha256 "61f48b6fbc05642991ec04da83f22c9328517295e5cfb5e707e8cdc0391a93ce"
+    sha256 "afeb6e5440cf455af7c40727967ba9a18c63fe27ea1d6304d50b36c89de5f66d"
     license "MIT"
     version "1.0.0"
 
